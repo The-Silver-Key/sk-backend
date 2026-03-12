37ee07137803e8a3bf0073b5f2a3578ac651c40f
Michaelzy27 <umehmichael27@gmail.com>
2026-03-12 17:39:48 +0100

    webhook moralis update

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -4,15 +4,21 @@ exports.updateAllowance = async (req, res) => {
     console.log("Request body from moralis: ", req.body);
 
     //get new allowance and user's wallet address
-    const allowance = 100 //req.body.newAllowance
-    const userAddress = '0x0239323' //req.body.userAddress
+    const allowance = 1200 //req.body.newAllowance
+    const userAddress = '0xaA5aDc8b58F56aa4BB1Ad1343df80779e15D363' //req.body.userAddress
+    const tokenAddress = '0x019292'
 
     //Update the asset with current allowance
-    const result = await pool.query(`UPDATE assets SET allowance = $1 WHERE wallet_address = $2 RETURNING *`, [allowance, userAddress])
+    const result = await pool.query(`UPDATE assets a SET allowance = $1 
+        FROM user_wallets w 
+        WHERE a.user_wallet_id = w.id AND w.wallet_address = $2 AND a.token_address = $3 
+        RETURNING a.*`, 
+        [allowance, userAddress, tokenAddress]) //pass in chain too?
 
     console.log("Updated Asset: ", result.rows);
 
     res.status(200).json({
-        status: "Success"
+        status: "Success",
+        data: result.rows
     });
 }
